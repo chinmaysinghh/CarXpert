bb4999fcfaa7b79cc1a35e593d4129363c4a84f4
chinmaysinghh <chinmaysingh240204@gmail.com>
2024-07-08 00:36:51 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 px-5 text-center" data-aos="fade-up" data-aos-anchor="#contact">
-      <p>&copy; 2024 Car Service Center. All rights reserved.</p>
+      <p>&copy; 2024 CarXpert. All rights reserved.</p>
     </footer>
   );
 }
