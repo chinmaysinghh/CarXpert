133da2d12284e159a114f67a83f8e0554f488cdc
chinmaysinghh <chinmaysingh240204@gmail.com>
2024-07-10 23:43:50 +0530

    hero problem is fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 
 function Hero() {
+  const desktopVideoRef = useRef(null);
+  const mobileVideoRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth < 768;
+      if (isMobile) {
+        mobileVideoRef.current.play();
+        desktopVideoRef.current.pause();
+      } else {
+        desktopVideoRef.current.play();
+        mobileVideoRef.current.pause();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Call initially to set the correct video state
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section id="hero" className="relative bg-white h-screen flex flex-col justify-center items-center text-center text-white px-5" data-aos="fade-in">
       <div className="absolute inset-0 w-full h-full">
         <video
+          ref={desktopVideoRef}
           className="hidden md:block absolute inset-0 w-full h-full object-cover"
           src="videos/bgdesktop.mp4"
           autoPlay
@@ -15,8 +39,9 @@ function Hero() {
           playsInline
         ></video>
         <video
+          ref={mobileVideoRef}
           className="block md:hidden absolute inset-0 w-full h-full object-cover"
-          src="videos/bgdesktop.mp4"
+          src="videos/bgmobile.mp4"
           autoPlay
           loop
           muted
