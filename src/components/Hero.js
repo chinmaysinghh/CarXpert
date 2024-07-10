663fae03957a9e7a83cea672719f647747853ae8
chinmaysinghh <chinmaysingh240204@gmail.com>
2024-07-10 19:16:39 +0530

    hero background changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
       <div className="absolute inset-0 w-full h-full">
         <video
           className="hidden md:block absolute inset-0 w-full h-full object-cover"
-          src="videos/bgdesktop.mp4"
+          src="videos/bgvideo2.mp4"
           autoPlay
           loop
           muted
@@ -16,7 +16,7 @@ function Hero() {
         ></video>
         <video
           className="block md:hidden absolute inset-0 w-full h-full object-cover"
-          src="videos/bgmobile.mp4"
+          src="videos/bgvideo2.mp4"
           autoPlay
           loop
           muted
